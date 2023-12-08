52f72f650ad03b2d7b236b4b2684bd7941adc59d
bricuahu <143514424+bricuahu@users.noreply.github.com>
2023-12-08 09:52:08 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function changeColor(squareId){
+    var square = getElementById(squareId);
+    var randomColor =  getRandomColor();
+    square.style.backgroundColor = randomColor;
+}
+
+
+function getRandomColor(); {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6;  i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color; 
+}
+
+function flashColors() {
+    var flashes = document.querySelectorAll('flash')
+}
+ 
